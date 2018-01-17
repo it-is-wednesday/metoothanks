402d8294c097387f37586a96e9cb97e20b5e3dfd
tag-them <qoev@protonmail.com>
2018-01-17 12:03:00 +0200

    util update

--- app/utils/src/clojure/utils/core.clj
+++ app/utils/src/clojure/utils/core.clj
@@ -3,10 +3,21 @@
     (:gen-class
         :methods [^{:static true} [fitToWidth [int String int interfaces.WidthCalcFuncComposer] String]]))
 
-(defn -main
-    "I don't do a whole lot ... yet."
-    [& args]
-    (println "Hello, World!"))
+(defn last-line [text]
+    (-> text
+        (#(clojure.string/split % #"\n"))
+        last))
+
+(defn longer [text1 text2]
+    (> (clojure.string/siz)))
+
+(defn fit-to-width [width text text-size width-calc-func words]
+    (if (empty? words)
+        (.trim text)
+        (let [new-line-width  (str (last-line text) " " (first words))
+              new-width-exceeds (> (width-calc-func ) width)
+              new-text (str text " " (first words))]
+            (recur width new-text text-size width-calc-func (rest words)))))
 
 (defn -fitToWidth [width text text-size width-calc-func]
     1)
